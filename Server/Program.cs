0501fac73088fb639e7400797401c0482960c8c5
witodev <szwed.witold@gmail.com>
2016-11-01 13:44:28 +0100

    fix

--- Server/Program.cs
+++ Server/Program.cs
@@ -13,18 +13,12 @@ namespace Server
         {
             var server = new AsyncServer();
             server.StartListening();
-
-            Console.WriteLine("Exit...");
-            Console.ReadKey();
-            var server = new SyncServer();
-            server.OnRespond += Server_OnRespond;
-            server.StartListening();
-
+            
             Console.WriteLine("Exit...");
             Console.ReadKey();
         }
 
-        private static void Server_OnRespond(object sender, StringBuilder e)
+        private void Server_OnRespond(object sender, StringBuilder e)
         {
             e.Append("\tServer: ");
             var now = DateTime.Now.Millisecond.ToString();
